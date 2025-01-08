6ef48d497030d4fa29da8ac0aaf0ee7dd1d505c3
hao <hao@gmail.com>
2025-01-08 14:46:59 +0700

    JavaScript Fundamentals Part 1 Cau 2

--- lab1.js
+++ lab1.js
@@ -9,3 +9,8 @@ var heightJohn = 1.95
 var BMIJohn = massJohn/(heightJohn*heightJohn)
 
 var markHigherBMI = BMIMark > BMIJohn 
+//2
+if(markHigherBMI ==true)
+    console.log(`BMI của Mark (${BMIMark.toFixed(1)}) is cao hơn của John (${BMIJohn.toFixed(1)})!`)
+else
+    console.log(`BMI của John (${BMIJohn.toFixed(1)}) is cao hơn của Mark (${BMIMark.toFixed(1)})!`);
