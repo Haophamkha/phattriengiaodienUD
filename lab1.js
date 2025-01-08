aac37e24f10cf2dbf25e054dc5f4832d59302157
hao <hao@gmail.com>
2025-01-08 22:25:00 +0700

    Developer Skills & Editor Setup

--- lab1.js
+++ lab1.js
@@ -132,6 +132,20 @@ else
         return sum / arr.length
     }
     console.log("Average tip:", calcAvarage(tips))
+    //Developer Skills & Editor Setup
+    function printForecast(arr) {
+        let result = ''
+        for (let i = 0; i < arr.length; i++) {
+            result += `${arr[i]}°C in ${i + 1} days... `
+        }
+        console.log('...' + result)
+    }
+    data1 = [17, 21, 23]
+    data2 = [12, 5, -5, 0, 4]
+
+    printForecast(data1)
+    printForecast(data2)
+
 
 
 
