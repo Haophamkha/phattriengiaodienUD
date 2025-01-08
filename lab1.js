ba63d284b9ba4064d13f8cbee1443d0a6cf5f183
hao <hao@gmail.com>
2025-01-08 14:38:54 +0700

    JavaScript Fundamentals Part 1 Cau 1

---
+++ lab1.js
@@ -0,0 +1,11 @@
+//JF1
+//1
+var massMark = 78
+var heightMark = 1.69
+var BMIMark = massMark/(heightMark*heightMark)
+
+var massJohn = 92
+var heightJohn = 1.95
+var BMIJohn = massJohn/(heightJohn*heightJohn)
+
+var markHigherBMI = BMIMark > BMIJohn 
